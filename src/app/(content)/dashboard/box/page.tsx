cbb3e92f833d76739076e5837a2c2724c9be9e76
David Botello <dabocher@gmail.com>
2024-03-24 08:37:31 +0100

    Change cards routes and display content on second children

---
+++ src/app/(content)/dashboard/box/page.tsx
@@ -0,0 +1,11 @@
+import Box from "@/components/Box";
+
+const BoxPage = () => {
+  return (
+    <div className="h-full w-full">
+      <Box />
+    </div>
+  );
+};
+
+export default BoxPage;
--- src/app/(content)/dashboard/communities/page.tsx
+++ src/app/(content)/dashboard/communities/page.tsx
@@ -1,7 +1,12 @@
+import Communities from "@/components/Communities";
 import React from "react";
 
 const CommunitiesPage = () => {
-  return <div>CommunitiesPage</div>;
+  return (
+    <div className="h-full w-full ">
+      <Communities />
+    </div>
+  );
 };
 
 export default CommunitiesPage;
