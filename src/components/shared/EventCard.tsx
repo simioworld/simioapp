96deb2124b687ed933a2bf36c3a80e00eb59bc9a
David Botello <dabocher@gmail.com>
2024-03-27 15:19:26 +0100

    Modify path routes and some tailwind classes

--- src/components/shared/EventCard.tsx
+++ src/components/shared/EventCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Id } from "../../../convex/_generated/dataModel";
 import Link from "next/link";
+import { staatliches } from "@/constants";
 
 export interface EventStructure {
   _id: Id<"events">;
@@ -40,8 +41,8 @@ const EventCard = ({ event }: { event: EventStructure }) => {
   const day = event.startDate?.split("-")[2];
 
   return (
-    <Link href={`/events/${event._id}`}>
-      <article className="text-neutral-200 flex flex-col  shadow-md rounded-lg  overflow-hidden hover:scale-105 duration-500 transition-all">
+    <Link href={`/dashboard/${event._id}`}>
+      <article className="text-neutral-200 flex flex-col  shadow-md rounded-lg  overflow-hidden hover:scale-105 duration-500 transition-all w-48 sm:w-52 md:w-56">
         <div>
           <Image
             src={`/assets/images/simulators/${event.simulator}.webp`}
@@ -52,42 +53,48 @@ const EventCard = ({ event }: { event: EventStructure }) => {
             priority
           />
         </div>
-        <div className="p-4 py-2 flex flex-col gap-1 bg-transparent/20 backdrop-blur-sm">
-          <div className="flex gap-1 items-center justify-between">
-            <h2 className="text-slate-200 font-bold text-lg">
-              {event.eventType}
-            </h2>
-            <Image
-              src="/assets/icons/heart.svg"
-              alt="corazon"
-              width={16}
-              height={16}
-              className="cursor-pointer"
-            />
-          </div>
-          <div className="flex gap-1 justify-between  ">
+        <div className="p-4 py-2 flex flex-col gap-2 bg-transparent/30 ">
+          <div className="flex flex-col">
+            <div className="flex gap-1 items-center justify-between">
+              <h2
+                className={`${staatliches.className} text-orange-500/90 font-bold text-xl`}
+              >
+                {event.eventType}
+              </h2>
+              <Image
+                src="/assets/icons/heart.svg"
+                alt="corazon"
+                width={12}
+                height={12}
+                className="cursor-pointer"
+              />
+            </div>
             <div className="flex gap-1 justify-between  ">
-              <div className="flex flex-col gap-0">
-                <div className="flex gap-2 items-center">
-                  <Image
-                    src="/assets/icons/location.svg"
-                    alt="circuito"
-                    width={12}
-                    height={12}
-                    className=""
-                  />
-                  <p className="text-neutral-200 text-sm ">{event.location}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <div className="flex gap-2 items-center">
+              <div className="flex gap-1 justify-between  ">
+                <div className="flex flex-col gap-0">
+                  <div className="flex gap-1 items-center">
                     <Image
-                      src="/assets/icons/car.svg"
-                      alt="coche"
-                      width={14}
-                      height={14}
+                      src="/assets/icons/location.svg"
+                      alt="circuito"
+                      width={12}
+                      height={12}
                       className=""
                     />
-                    <p className="text-xs">{event.carCategory}</p>
+                    <p className="text-neutral-200 text-xxs ">
+                      {event.location}
+                    </p>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <div className="flex gap-1 items-center">
+                      <Image
+                        src="/assets/icons/car.svg"
+                        alt="coche"
+                        width={12}
+                        height={12}
+                        className=""
+                      />
+                      <p className="text-xxs">{event.carCategory}</p>
+                    </div>
                   </div>
                   <div className="flex gap-1 items-center">
                     <Image
@@ -97,15 +104,13 @@ const EventCard = ({ event }: { event: EventStructure }) => {
                       height={12}
                       className="text-neutral-200"
                     />
-                    <p className="text-xs">{event.slots}</p>
+                    <p className="text-xxs">{event.slots}</p>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="flex gap-0 items-center">
-              <div className=" flex flex-col gap-0 ">
-                <div className=" flex justify-end gap-1 ">
+              <div className=" flex flex-col ">
+                <div className=" flex justify-between gap-1 ">
                   <Image
                     src="/assets/icons/clock.svg"
                     alt="reloj"
@@ -113,9 +118,9 @@ const EventCard = ({ event }: { event: EventStructure }) => {
                     height={12}
                     className="text-neutral-200"
                   />
-                  <p className="text-xs text-right ">{event.duration} min</p>
+                  <p className="text-xxs text-right ">{event.duration} min</p>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex gap-1 justify-between">
                   <Image
                     src="/assets/icons/calendar.svg"
                     alt="calendario"
@@ -123,15 +128,14 @@ const EventCard = ({ event }: { event: EventStructure }) => {
                     height={12}
                     className="text-neutral-200"
                   />
-                  <p className="text-xs">
+                  <p className="text-xxs">
                     {day}/{month}
                   </p>
                 </div>
-                <p className="text-xs text-right">{event.startTime} h</p>
+                <p className="text-xxs text-right">{event.startTime} h</p>
               </div>
             </div>
           </div>
-
           <div className="flex items-center justify-between gap-2">
             <div className="flex items-center gap-1">
               <Image
@@ -139,13 +143,13 @@ const EventCard = ({ event }: { event: EventStructure }) => {
                   .toLowerCase()
                   .replaceAll(" ", "")}.webp`}
                 alt={event.community}
-                width={14}
-                height={14}
+                width={12}
+                height={12}
                 className="rounded-full h-4 w-4"
               />
-              <p className="text-sm capitalize ">{event.community}</p>
+              <p className="text-xxs capitalize ">{event.community}</p>
             </div>
-            <div className="text-xxs">Creado por {event.userName}</div>
+            {/*   <div className="text-xxs">Creado por {event.userName}</div> */}
           </div>
         </div>
       </article>
