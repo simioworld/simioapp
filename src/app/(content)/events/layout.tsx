5a85e349696f1e81f16ad21265f883f973034e83
David Botello <dabocher@gmail.com>
2024-03-27 15:05:23 +0100

    Delete event path

--- src/app/(content)/events/layout.tsx
+++
@@ -1,9 +0,0 @@
-const EventsLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="flex justify-center w-full p-6">{children}</div>;
-};
-
-export default EventsLayout;
