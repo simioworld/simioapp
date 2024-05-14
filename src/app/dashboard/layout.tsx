b3490f6db2120f1c5ce292903d28774446ebfd66
David Botello <dabocher@gmail.com>
2024-05-14 15:56:22 +0200

    Delete functional code on dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,50 +1,15 @@
-import Header from "@/components/shared/Header";
-import CardLinksItems from "@/components/shared/NavComponents/CardLinks";
-import NavEvents from "@/components/shared/NavComponents/NavEvents";
-import Image from "next/image";
-import { Suspense } from "react";
-import Spinner from "@/components/shared/Spinner";
+import Header from "@/components/shared/header/Header";
 
-const DashboardLayout = ({
-  children,
-  events,
-}: {
-  children: React.ReactNode;
-  events: React.ReactNode;
-}) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gradient-to-b w-full from-slate-700 via-slate-800 to-slate-700 min-h-screen flex flex-col gap-4 p-4 px-6">
-      <Header />
+    <div className=" w-full bg-gradient-to-b from-slate-700 via-slate-800  to-slate-700 flex justify-center min-h-screen">
+      <div className=" w-full lg:w-3/4 flex flex-col gap-4 p-4 px-6">
+        <Header />
 
-      <main className=" w-full flex flex-row gap-2 sm:gap-4 min-h-[80vh]">
-        <section
-          className="relative p-4 px-6 xs:p-8  bg-gradient-to-br from-slate-600/80 to-[#374A67]/95 rounded-md shadow-xl flex flex-col items-center xs:items-start xs:flex-row xs:justify-start lg:justify-center gap-y-4 lg:gap-6 w-2/3 flex-grow-1
-        "
-        >
-          <Image
-            src={"/assets/images/fondo.webp"}
-            alt="Setup de un simracer"
-            fill
-            style={{ objectFit: "cover", objectPosition: "top" }}
-            className="opacity-10  "
-          />
-          <aside className="self-start  w-full xs:w-2/5 lg:w-1/5">
-            <NavEvents />
-          </aside>
-          <div className="w-full xs:w-3/5 lg:w-4/5">
-            <Suspense fallback={<Spinner />}>
-              <section>{events}</section>
-            </Suspense>
-          </div>
-        </section>
-
-        <aside className=" w-1/3 min-h-full ">
-          <CardLinksItems />
-          <div className="flex-1 h-full bg-slate-700 rounded-md shadow-xl hidden">
-            {children}
-          </div>
-        </aside>
-      </main>
+        <main className=" w-full  flex flex-row gap-2 sm:gap-4 rounded-md  shadow-md">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,48 @@
-const page = () => {
+import EventList from "@/components/shared/events/EventList";
+import Spinner from "@/components/shared/Spinner";
+import { Suspense } from "react";
+import { SearchBar } from "../(clone)/SearchBar";
+import { staatliches } from "@/constants";
+
+const page = (/* {
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+    firstItem?: string;
+    itemsPerPage?: string;
+    favorites?: string;
+  };
+} */) => {
+  /*   const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const firstItem = Number(searchParams?.firstItem) || 0;
+  const itemsPerPage = 4;
+ */
   return (
-    <div className="h-full hidden sm:flex sm:flex-col justify-center items-center p-4 text-neutral-100/50">
-      <h4>Ultimas noticias</h4>
-      <p>Comunidades añadidas</p>
-    </div>
+    <section className=" flex flex-col items-center w-full space-y-3 lg:space-y-5 lg:px-6">
+      {/* 
+      <h2
+        className={`${staatliches.className} z-20 hidden xs:flex  text-4xl xs:text-3xl text-slate-800/90 font-bold xs:justify-center   `}
+      >
+        Próximos eventos
+      </h2>
+      <div className="w-full flex items-center justify-center  lg:max-w-[420px] ">
+        <SearchBar placeholder={"Buscar evento..."} />
+      </div>
+
+      <div className="w-full">
+        <Suspense key={query + currentPage} fallback={<Spinner />}>
+          <EventList
+            query={query}
+            currentPage={currentPage}
+            firstItem={firstItem}
+            itemsPerPage={itemsPerPage}
+          />
+        </Suspense>
+      </div> */}
+    </section>
   );
 };
 
