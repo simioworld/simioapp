3088e9e1e78cfa22bb93dc19feade63ce158e7a2
David Botello <dabocher@gmail.com>
2024-05-14 15:36:24 +0200

    Create constant for convex backend

---
+++ convex/constants.ts
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+
+export const simulatorUnionValues = v.union(
+  v.literal("ACC"),
+  v.literal("Assetto Corsa"),
+  v.literal("Automobilista 2"),
+  v.literal("Dirt Rally 2"),
+  v.literal("F1 23"),
+  v.literal("Gran Turismo 7"),
+  v.literal("iRacing"),
+  v.literal("RaceRoom"),
+  v.literal("rFactor 2")
+);
+
+export const carCategoryUnionValues = v.union(
+  v.literal("GT2"),
+  v.literal("GT3"),
+  v.literal("GT4"),
+  v.literal("Porsche Cup"),
+  v.literal("Otra")
+);
+
+export const eventTypeUnionValues = v.union(
+  v.literal("Campeonato"),
+  v.literal("Carrera"),
+  v.literal("Entrenamiento"),
+  v.literal("Reto"),
+  v.literal("Resistencia")
+);
