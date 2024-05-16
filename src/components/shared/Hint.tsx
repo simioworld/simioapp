adb644184895e76166e766bbefe8dbbf71e8ab94
David Botello <dabocher@gmail.com>
2024-05-16 16:50:13 +0200

    Create Hint component

---
+++ src/components/shared/Hint.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+
+interface HintProps {
+  className?: string;
+  label: string;
+  simulators?: string[];
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+const Hint = ({
+  className,
+  label,
+  children,
+  side = "bottom",
+  align = "start",
+  sideOffset = 4,
+  alignOffset = 4,
+  simulators = ["ACC"],
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className={`${className} bg-slate-800 text-slate-100`}
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+        >
+          <h3 className="font-semibold capitalize">{label}</h3>
+          <p className="font-light">{simulators.join(" / ")}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default Hint;
