b69183a68ef26cae04f5ba38b4a709c385e359e8
David Botello <dabocher@gmail.com>
2024-05-16 16:50:41 +0200

    Create Subheader component

---
+++ src/components/shared/SubHeader.tsx
@@ -0,0 +1,27 @@
+import { staatliches } from "@/constants";
+import Link from "next/link";
+import React from "react";
+
+interface NavEventsProps {
+  children?: React.ReactNode;
+  title: string;
+  path?: string;
+}
+const SubHeader = ({ children, title, path }: NavEventsProps) => {
+  return (
+    <header className="relative px-4 py-2 w-full flex flex-row gap-4 items-center justify-between shadow-md  opacity-80">
+      <div className="flex flex-row gap-4 items-center">
+        <Link href={`/dashboard/${path}`}>
+          <h2
+            className={`${staatliches.className}  xs:flex  text-4xl text-orange-500 font-bold xs:justify-center`}
+          >
+            {title}
+          </h2>
+        </Link>
+        {children}
+      </div>
+    </header>
+  );
+};
+
+export default SubHeader;
