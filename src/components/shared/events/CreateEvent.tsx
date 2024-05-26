21cf254c477af25846da7d8d7bb2918d9579f5c1
David Botello <dabocher@gmail.com>
2024-05-26 10:55:14 +0200

    Change style on Create-event button

--- src/components/shared/events/CreateEvent.tsx
+++ src/components/shared/events/CreateEvent.tsx
@@ -2,15 +2,15 @@ import { Plus } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-const CreateEvent = () => {
+const CreateEventIcon = () => {
   return (
     <Link
       href={"/dashboard/events/create-event"}
-      className="absolute right-2 lg:right-4 top-1/2 -translate-y-1/2 text-orange-500 bg-slate-800 rounded-md p-1 hover:rounded-full hover:bg-slate-200/100 transition-all duration-500"
+      className="absolute right-2 lg:right-4 top-1/2 -translate-y-1/2 text-orange-500  hover:rounded-full hover:bg-slate-200/100 transition-all duration-500"
     >
       <Plus />
     </Link>
   );
 };
 
-export default CreateEvent;
+export default CreateEventIcon;
