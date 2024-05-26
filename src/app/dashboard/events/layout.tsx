55d66a78b1ade658e27f3941c3dd4413eeb56a30
David Botello <dabocher@gmail.com>
2024-05-26 10:57:07 +0200

    Change padding and naming on CreateEventIcon

--- src/app/dashboard/events/layout.tsx
+++ src/app/dashboard/events/layout.tsx
@@ -3,18 +3,17 @@
 import Hint from "@/components/shared/Hint";
 import ImageBackground from "@/components/shared/ImageBackground";
 import SubHeader from "@/components/shared/SubHeader";
-import CreateEvent from "@/components/shared/events/CreateEvent";
+import CreateEventIcon from "@/components/shared/events/CreateEvent";
 import NavEvents from "@/components/shared/events/NavEvents";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 const EventLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-
   return (
     <section className="overflow-hidden  h-full relative flex flex-col items-center w-full   ">
       <ImageBackground route={pathname} />
-      <div className="flex flex-row justify-start w-full ">
+      <div className="flex flex-row justify-start w-full px-4">
         <SubHeader title="Eventos" path={pathname}>
           <NavEvents />
         </SubHeader>
@@ -26,11 +25,11 @@ const EventLayout = ({ children }: { children: React.ReactNode }) => {
             sideOffset={28}
             alignOffset={10}
           >
-            <CreateEvent />
+            <CreateEventIcon />
           </Hint>
         </div>
       </div>
-      <div className="flex flex-col px-4 sm:flex-row items-center  pt-10 pb-4 w-full ">
+      <div className="flex flex-col px-4 sm:flex-row items-center pt-4 lg:pt-10 pb-4 w-full ">
         {children}
       </div>
     </section>
