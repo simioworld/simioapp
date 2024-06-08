32eddb6c23276790052749477090e910e7f0cdff
David Botello <dabocher@gmail.com>
2024-06-08 09:25:36 +0200

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import { montserrat } from "@/constants";
 export const metadata: Metadata = {
   title: "Simoworld",
   description: "Planificador de eventos de simracing en español",
+  icons: {
+    icon: "/icon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +21,7 @@ export default function RootLayout({
     <html lang="es">
       <body
         suppressHydrationWarning={true}
-        className={`${montserrat.className} select-none   bg-gradient-to-b  from-neutral-400/40 to-neutral-100 min-h-screen`}
+        className={`${montserrat.className} select-none bg-gradient-to-b from-neutral-400/40 to-neutral-100 min-h-screen`}
       >
         <ClerkConvexProvider>
           <ThemeProvider
